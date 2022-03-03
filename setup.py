e520cb4583892d45b523cc5191eae17f3b1c89e8
Zane Fink <finkzane@gmail.com>
2022-03-03 09:05:56 -0600

    initial setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PyCCS",
+    version="0.01",
+    packages = ['pyccs'],
+    install_requires = [""],
+    author = "Zane Fink",
+    author_email = "zanef2@illinois.edu",
+    description = "Python CCS Interface",
+    long_description = "",
+    license = "",
+    keywords = "pytest testing",
+    url = "",
+#    entry_points = {
+#        'pytest11': [
+#            'pytest_charm = pytest_charm',
+#        ]
+#    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
